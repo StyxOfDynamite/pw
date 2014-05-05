65a5f7069a27ee78aaa2930e386a92d2db9aa8a6
Luke Storer <luke.storer@jadu.net>
2014-05-05 16:21:06 +0100

    refactored out individual constraints to variables to begin extracting them to options

--- js/plugin.js
+++ js/plugin.js
@@ -1,16 +1,14 @@
 //plugin version 0.1
 (function($) {
-    $.fn.showLinkLocation = function() {
-        this.filter("a").each(function() {
-            var link = $(this);
-            link.append(" (" + link.attr("href") + ")");
-        });
-        return this;
-    };
-}(jQuery));
-(function($) {
     $.fn.addPasswordConstraints = function() {
-        var passwordContstraints = "<ul><li>8 characters</li><li>Mixed case</li></ul>";
+
+        var eightPlus = '<li>8 characters</li>';
+        var uppercase = '<li>Uppercase character</li>';
+        var lowercase = '<li>Lowercase character</li>';
+        var numbers = '<li>Number character</li>';
+        var punctuation = '<li>Special character</li>';
+
+        var passwordContstraints = '<ul>' + eightPlus + uppercase + lowercase + numbers + punctuation + '</ul>';
         $(passwordContstraints).insertAfter($(this));
         return this;
     };
